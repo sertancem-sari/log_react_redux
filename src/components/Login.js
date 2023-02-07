86aca801e033fe1b74c2ce8ca02feee3ec769973
SISTEK NETWORK <SISTEK_NETWORK@atv.com.tr>
2023-02-07 15:02:08 +0300

    login page added

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,31 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <form className="login-wrap">
+	      <div className="login-html">
+		      <input id='username' type="radio" name="tab" className="sign-in" checked/>
+          <label htmlFor='username' className="tab">GİRİŞ</label>
+		      {/* <input id="tab-2" type="radio" name="tab" className="sign-up"/>
+          <label for="tab-2" className="tab">Sign Up</label> */}
+          <div className="login-form">
+            <div className="sign-in-htm">
+              <div className="group">
+                <label for="user" className="label">Kullanıcı Adı</label>
+                <input id="user" type="text" className="input"/>
+				      </div>
+              <div className="group">
+                <label for="pass" className="label">Parola</label>
+                <input id="pass" type="password" className="input" data-type="password"/>
+              </div>
+              <div className="group">
+                <button className='button'>Giriş Yap</button>
+              </div>
+			      </div>
+		      </div>
+        </div>
+      </form>
+    </>
   )
 }
 
