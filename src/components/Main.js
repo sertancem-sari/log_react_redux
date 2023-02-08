bdffa3115debe07c9c6f032df995de24e6bdd9aa
SISTEK NETWORK <SISTEK_NETWORK@atv.com.tr>
2023-02-08 14:56:43 +0300

    frontend main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Main = () => {
+  const date = new Date()
+  const today = new Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(date)
   return (
-    <div>Main</div>
+    <section className="welcome">
+
+            <p>{today}</p>
+
+            <h1>Hoşgeldiniz!</h1>
+
+            <p><Link to="/dash/logs">Logları görüntüle</Link></p>
+            <p><Link to="/dash/users">Kullanıcı ayarları</Link></p>
+
+            <p><Link to="/dash/users/new">Yeni Kullanıcı Ekle</Link></p>
+
+        </section>
   )
 }
 
---
+++ src/features/users/EditUser.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const EditUser = () => {
+  return (
+    <>
+      <form className="login-wrap">
+	      <div className="login-html">
+		      <input id='username' type="text" name="username" className="sign-in" checked/>
+          <label htmlFor='username' className="welcome-button">KULLANICI AYARLARI</label>
+          <div className="login-form">
+            <div className="sign-in-htm">
+              <div className="group">
+                <label for="username" className="label">Kullanıcı Adı</label>
+                <input 
+                  id="username"
+                  name="username"
+                  type="text"
+                  autoComplete="off"
+                  className="input"
+                />
+				      </div>
+              <div className="group">
+                <label for="password" className="label">Parola</label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  className="input" 
+                  data-type="password"
+                />
+              </div>
+              <div className="group">
+                <label for="pass" className="label">ROL</label>
+                <input
+                  id="roles"
+                  name="roles"
+                  multiple={true}
+                  size="3"  
+                  className="input"
+                />
+              </div>
+              <div className="group">
+                <button className='button'>Kaydet</button>
+              </div>
+			      </div>
+		      </div>
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default EditUser
